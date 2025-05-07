38efdd6593f8fce52979233cc6b1c1ede35cd7c4
nalikanon <apisityambangyang@gmail.com>
2025-05-08 04:35:18 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'My website',
+  title: 'My Introduction',
   description: 'Personal portfolio and introduction of Apisit Yambangyang, AI Developer and Software Engineer',
 }
 
